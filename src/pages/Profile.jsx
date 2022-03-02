e92d87b86af68b095052a4cb3bfcf4f190392bcb
danielp345 <danielp345@gmail.com>
2022-03-02 18:19:17 +0100

    add listing item

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useNavigate, Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { getAuth, updateProfile } from "firebase/auth"
 import { doc, updateDoc } from "firebase/firestore"
 import { db } from "../firebase.config"
